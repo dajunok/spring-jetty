075160a696538d73bedc532c423db43e664bbf2c
liulijun <1937249556@qq.com>
2018-12-23 18:19:40 +0800

    提交：liu.web.SpittleRepositoryImp.java

---
+++ src/main/java/liu/web/SpittleRepositoryImp.java
@@ -0,0 +1,29 @@
+package liu.web;
+
+import java.util.List;
+
+public class SpittleRepositoryImp implements SpittleRepository {
+	private List<Spittle> list;
+	
+
+	public List<Spittle> findRecentSpittles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Spittle> findSpittles(long max, int count) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Spittle findOne(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void save(Spittle spittle) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
